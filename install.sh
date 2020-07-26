a3846f11f4884cd6bb18c483d301f271dcc32008
Dmitry Surin <dmitry.surin@gmail.com>
2020-07-26 18:59:06 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
+
+# Show extra debug info
+set -e
+
 echo 'We are setting up'
+echo 'Installing Dotfiles'
 cd ~
 git init && \
 git remote add origin https://github.com/bedware/dotfiles.git && \
 git pull origin master && \
 git submodule update --init -j 2 && \
 chmod 0600 .ssh/id_*
-echo 'Common things done'
 
+echo 'Installing zsh'
 sudo apt install -y zsh
-echo 'Zsh installed'
 
+echo 'Installing oh-my-zsh'
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash -s -- --unattended
-echo 'Installing Oh-my-zsh done'
 
 curl -fsSL https://starship.rs/install.sh | bash -s -- -y
 echo 'eval "$(starship init zsh)"' >> ~/.zshrc
-echo 'Installing Starship done'
+echo 'Installing Starship'
 
-echo 'Post-step: run in the console command below:'
-echo 'chsh -s $(which zsh)'
-echo 'Then relogin'
-# zsh
+echo 'Switching to zsh'
+chsh -s $(which zsh)
+$(which zsh)
 
 # Get the IP address of the host from /etc/resolv.conf
 # export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
