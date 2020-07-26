eb7485f60127f95f24d69eb958ed09c437717c0c
Dmitry Surin <dmitry.surin@gmail.com>
2020-07-26 03:21:06 +0300

    Updating install script

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 echo We are setting up
-
 cd ~
 git init && \
 git remote add origin https://github.com/bedware/dotfiles.git && \
 git pull origin master && \
 git submodule update --init -j 2 && \
 chmod 0600 .ssh/id_*
+echo 'Common things done'
+
+sudo apt install -y zsh
+chsh -s $(which zsh)
+$(which zsh)
+echo `zsh --version` installed
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'Installing Oh-my-zsh done'
+
+curl -fsSL https://starship.rs/install.sh | bash
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+eval "$(starship init zsh)"
+echo 'Installing Starship done'
 
 echo Done
 
